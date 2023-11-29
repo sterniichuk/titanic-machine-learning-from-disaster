e3428dcb06d1961b1106789e478d8425fe3d9914
stern <sterniichuk.serhii@gmail.com>
2023-11-29 19:25:09 +0200

    feat: predicting on the test dataset

--- titanic-machine-learning-from-disaster/src/main/java/Main.java
+++ titanic-machine-learning-from-disaster/src/main/java/Main.java
@@ -1,6 +1,5 @@
-import org.tribuo.Model;
-import org.tribuo.MutableDataset;
-import org.tribuo.Trainer;
+import com.opencsv.CSVWriter;
+import org.tribuo.*;
 import org.tribuo.classification.Label;
 import org.tribuo.classification.LabelFactory;
 import org.tribuo.classification.evaluation.LabelEvaluator;
@@ -8,28 +7,55 @@ import org.tribuo.classification.sgd.linear.LogisticRegressionTrainer;
 import org.tribuo.data.csv.CSVLoader;
 import org.tribuo.evaluation.TrainTestSplitter;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
-
+import java.util.List;
 public class Main {
+    static final String pathToFolder = "C:\\Users\\stern\\Documents\\EducationLocal\\AI\\rgr\\data\\";
+
     public static void main(String[] args) throws IOException {
-        var path = Paths.get("C:\\Users\\stern\\Documents\\EducationLocal\\AI\\rgr\\data\\prepared_train_data.csv");
-        var labelFactory = new LabelFactory();
-        var csvLoader = new CSVLoader<>(labelFactory);
-        var titanicSource = csvLoader.loadDataSource(path, "Survived");
+        var titanicSource = getSource(pathToFolder + "prepared_train_data.csv", "Survived");
         var titanicSplitter = new TrainTestSplitter<>(titanicSource, 0.8, 1L);
         var trainingDataset = new MutableDataset<>(titanicSplitter.getTrain());
         var testingDataset = new MutableDataset<>(titanicSplitter.getTest());
-        Trainer<Label> trainer = new LogisticRegressionTrainer();
+        var trainer = new LogisticRegressionTrainer();
         long startTime = System.nanoTime();
         Model<Label> titanicModel = trainer.train(trainingDataset);
-        var evaluator = new LabelEvaluator();
-        var evaluation = evaluator.evaluate(titanicModel, testingDataset);
         long endTime = System.nanoTime();
-        long executionTime = endTime - startTime;
+        long trainingTime = endTime - startTime;
+        var evaluator = new LabelEvaluator();
+        startTime = System.nanoTime();
+        var evaluation = evaluator.evaluate(titanicModel, testingDataset);
+        endTime = System.nanoTime();
+        var executionTime = endTime - startTime;
         System.out.println(trainer);
         System.out.println(evaluation.toString());
-        System.out.println("Execution time in nanoseconds: " + executionTime);
+        System.out.println("Training time in milliseconds: " + (double) trainingTime / 1_000_000);
         System.out.println("Execution time in milliseconds: " + (double) executionTime / 1_000_000);
+        titanicSource = getSource(pathToFolder + "prepared_test_data.csv", "");
+        List<Prediction<Label>> predictions = titanicModel.predict(new MutableDataset<>(titanicSource));
+        saveToFile(predictions);
+    }
+
+    private static void saveToFile(List<Prediction<Label>> predictions) throws IOException {
+        try (var writer = new CSVWriter(new FileWriter(pathToFolder + "java_result.csv"))) {
+            String[] headers = {"PassengerId", "Survived"};
+            writer.writeNext(headers, false);
+            for (int i = 0; i < predictions.size(); i++) {
+                Prediction<Label> p = predictions.get(i);
+                String id =Integer.toString(892 + i);
+                String value = p.getOutput().getLabel();
+                String[] row = {id, value};
+                writer.writeNext(row, false);
+            }
+        }
+    }
+
+    private static DataSource<Label> getSource(String pathToSource, String responseName) throws IOException {
+        var path = Paths.get(pathToSource);
+        var labelFactory = new LabelFactory();
+        var csvLoader = new CSVLoader<>(labelFactory);
+        return csvLoader.loadDataSource(path, responseName);
     }
 }
